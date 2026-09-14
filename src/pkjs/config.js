d90f57b95321fed5f752236514c48bc9eca634d6
Marco Maas <65452159+Moaske@users.noreply.github.com>
2026-09-14 17:13:19 +0200

    v1.2.1
    Some small fixes I forgot, and a working app icon confirmed

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -11,6 +11,7 @@ module.exports = [
         "messageKey": "FuelType",
         "defaultValue": "E10",
         "label": "Fuel type",
+        "description": "Fuel station/price data provided by ANWB© NL.<br>Copyright notice ©<br>Covers the follwing countries:<br>NL, BE, LU, DE, FR, ES, PT, IT, CH, DK, NO, SE, FI, GB, TR, UA, RU.",
         "options": [
           { "label": "E10 (Euro 95)", "value": "E10"    },
           { "label": "E5 (Super 98)", "value": "E5"     },
@@ -23,7 +24,7 @@ module.exports = [
         "messageKey": "StationCount",
         "defaultValue": "10",
         "label": "Stations in list",
-        "description": "How many nearby stations to show. Both are fetched in one request, so this does not affect data use.",
+        "description": "How many nearby stations to show. All are allready fetched in one request, so this does not affect data use and merely limits the list.",
         "options": [
           { "label": "10 stations", "value": "10" },
           { "label": "20 stations", "value": "20" },
